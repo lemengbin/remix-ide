6ef1c46ef62af25078f47cc9ef844a47249e5c96
lemengbin <lemengbin@163.com>
2019-05-24 13:17:48 +0800

    use Um address instead of Hex address in inputs

--- src/universal-dapp.js
+++ src/universal-dapp.js
@@ -20,6 +20,7 @@ var globalRegistry = require('./global/registry')
 var modalDialog = require('./app/ui/modaldialog')
 var typeConversion = remixLib.execution.typeConversion
 var confirmDialog = require('./app/execution/confirmDialog')
+var base58 = require('./base58')
 
 function UniversalDApp (opts, localRegistry) {
   this.event = new EventManager()
@@ -208,6 +209,47 @@ UniversalDApp.prototype.call = function (isUserAction, args, value, lookupOnly, 
     } else {
       logMsg = `call to ${args.contractName}.${(args.funABI.name) ? args.funABI.name : '(fallback)'}`
     }
+  }
+  var params = ''
+  try {
+    value = value.replace(/(^|,\s+|,)(\d+)(\s+,|,|$)/g, '$1"$2"$3') // replace non quoted number by quoted number
+    value = value.replace(/(^|,\s+|,)(0[xX][0-9a-fA-F]+)(\s+,|,|$)/g, '$1"$2"$3') // replace non quoted hex string by quoted hex string
+    params = JSON.parse('[' + value + ']')
+  } catch (e) {
+    self._deps.logCallback(`${logMsg} errored: Error encoding arguments: ${value} `)
+    return
+  }
+  if (params.length !== args.funABI.inputs.length) {
+    self._deps.logCallback(`${logMsg} errored: Invalid arguments: ${value} `)
+    return
+  }
+  var i = 0
+  if (args.funABI.inputs && args.funABI.inputs.length > 0) {
+    for (i = 0; i < args.funABI.inputs.length; i++) {
+      var inputType = args.funABI.inputs[i].type
+      if (inputType === 'address') {
+        var umAddress = params[i]
+        if ((/^(Um)?[1-9a-z]{33}$/i.test(umAddress)) && (/[^OIl]{1}/.test(umAddress))) {
+          params[i] = base58.UmAddressToHexAddress(umAddress)
+        } else {
+          self._deps.logCallback(`${logMsg} errored: Error encoding arguments: invalid address (${umAddress} `)
+          return
+        }
+      }
+    }
+  }
+  value = ''
+  for (i = 0; i < params.length - 1; i++) {
+    if (typeof params[i] === 'string') {
+      value += '"' + params[i] + '", '
+    } else {
+      value += params[i] + ', '
+    }
+  }
+  if (typeof params[i] === 'string') {
+    value += '"' + params[i] + '"'
+  } else {
+    value += params[i]
   }
   // contractsDetails is used to resolve libraries
   txFormat.buildData(args.contractName, args.contractAbi, self.data.contractsDetails, false, args.funABI, args.funABI.type !== 'fallback' ? value : '', (error, data) => {
